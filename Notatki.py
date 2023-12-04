1c810eab9b8af2de31bdc006df5d6b5888b5a1f2
Barney <dogier431@gmail.com>
2023-12-04 15:44:22 +0100

    wip: crud_dla_bazy_danych

--- Notatki.py
+++ Notatki.py
@@ -1,10 +1,27 @@
-import requests
+# import requests
+#
+# miasto = "zamosc"
+#
+#
+# def pogoda_z(miasto: str):
+#     url = f"https://danepubliczne.imgw.pl/api/data/synop/station/{miasto}"
+#     response = requests.get(url).json()
+#     print(response["id_stacji"])
+# pogoda_z(miasto)
 
-miasto = "zamosc"
 
+db_params = sqlalchemy.URL.create(
+    drivername="postgres"
+    username="postgres"
+    password="Zimos98103"
+    host="localhost"
+    database="postgres"
+    port=5432
+)
+engine=sqlalchemy.create_engine(db_params)
+connection = engine.connect()
 
-def pogoda_z(miasto: str):
-    url = f"https://danepubliczne.imgw.pl/api/data/synop/station/{miasto}"
-    response = requests.get(url).json()
-    print(response["id_stacji"])
-pogoda_z(miasto)
+sql_query_1=sqlalchemy.text("INSERT INTO public.my_table(name) VALUES('kepa')")
+connection.execute(sql_query_1)
+
+connection.commit()
