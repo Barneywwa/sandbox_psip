48c4e9a0a5dcfa20495888c0c6adf6707799b623
Barney <dogier431@gmail.com>
2023-12-14 11:51:30 +0100

    wip

--- Notatki.py
+++ Notatki.py
@@ -1,13 +1,13 @@
-# import requests
-#
-# miasto = "zamosc"
-#
-#
-# def pogoda_z(miasto: str):
-#     url = f"https://danepubliczne.imgw.pl/api/data/synop/station/{miasto}"
-#     response = requests.get(url).json()
-#     print(response["id_stacji"])
-# pogoda_z(miasto)
+import requests
+
+miasto = "zamosc"
+
+
+def pogoda_z(miasto: str):
+    url = f"https://danepubliczne.imgw.pl/api/data/synop/station/{miasto}"
+    response = requests.get(url).json()
+    print(response["id_stacji"])
+pogoda_z(miasto)
 
 
 db_params = sqlalchemy.URL.create(
