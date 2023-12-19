d04a1d0edf2cddece66fbda231c5760ccbbbe986
Barney <dogier431@gmail.com>
2023-12-19 16:02:09 +0100

    feat: notes

--- Notatki.py
+++ Notatki.py
@@ -11,17 +11,17 @@ pogoda_z(miasto)
 
 
 db_params = sqlalchemy.URL.create(
-    drivername="postgres"
-    username="postgres"
-    password="Zimos98103"
-    host="localhost"
-    database="postgres"
+    drivername="postgres",
+    username="postgres",
+    password="postgres",
+    host="localhost",
+    database="postgres",
     port=5432
 )
 engine=sqlalchemy.create_engine(db_params)
 connection = engine.connect()
 
-sql_query_1=sqlalchemy.text("INSERT INTO public.my_table(name) VALUES('kepa')")
-connection.execute(sql_query_1)
+# sql_query_1=sqlalchemy.text("INSERT INTO public.my_table(name) VALUES('kepa')")
+# connection.execute(sql_query_1)
 
-connection.commit()
+# connection.commit()
